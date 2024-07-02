e9b33775804634e7eb6673b8b422e4e6473629b9
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-07-03 00:02:55 +0800

    add top header

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,3 +1,9 @@
 <template>
-  
+  <v-container fill-height>
+    <v-row>
+      <v-col cols="12" class="text-center">
+        <div class="text-h4 text-white mt-2 mb-2">Grading Percentile Scores</div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
