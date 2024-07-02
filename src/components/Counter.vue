212b933feea6ac4f4b1d52350f9def0eb988e823
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-07-03 00:58:11 +0800

    counter functions

---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="text-center">
+      <v-text-field class="align-items-center" width="300" v-model.number="inputNumber" label="Enter the Total items"
+        @input="updateCounter" />
+    </div>
+    <v-row>
+      <v-col cols="12" sm="6" md="4" lg="1" v-for="n in counter" :key="n">
+        <v-card>
+          <v-card-body>{{ n }} - {{ ((n - 0) * 100) / inputNumber }}%</v-card-body>
+        </v-card>
+      </v-col>
+    </v-row>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+const inputNumber = ref(0)
+
+function updateCounter() {
+  counter.value = inputNumber.value
+}
+</script>
