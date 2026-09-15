edea0b04ee4574fc8b18d9e818df06c60dac49ee
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2026-09-15 09:45:57 +0800

    Revert "Add new feature or fix"
    This reverts commit fdeaa72e989daf2533142ed99db8e391aa404d2e.

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,9 +2,7 @@
   <v-container fill-height fluid>
     <v-row>
       <v-col cols="12" class="text-center">
-        <div class="text-h4 text-white mt-2 mb-2">
-          Grading Percentile Scores (edited)
-        </div>
+        <div class="text-h4 text-white mt-2 mb-2">Grading Percentile Scores</div>
       </v-col>
       <v-col cols="12" class="text-center">
         <Counter />
