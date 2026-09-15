fdeaa72e989daf2533142ed99db8e391aa404d2e
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2026-09-15 09:43:45 +0800

    Add new feature or fix

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,7 +2,9 @@
   <v-container fill-height fluid>
     <v-row>
       <v-col cols="12" class="text-center">
-        <div class="text-h4 text-white mt-2 mb-2">Grading Percentile Scores</div>
+        <div class="text-h4 text-white mt-2 mb-2">
+          Grading Percentile Scores (edited)
+        </div>
       </v-col>
       <v-col cols="12" class="text-center">
         <Counter />
